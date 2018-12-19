c634e55b3c25d11fa883aeca636acf3f96b0959c
todoval <todova@gmail.com>
2018-12-19 13:24:56 +0100

    First attempt at merging symbols into words

--- tabularOCR/src/ocr_process/process.cpp
+++ tabularOCR/src/ocr_process/process.cpp
@@ -27,35 +27,26 @@ namespace ocr
 		return pixd;
 	}
 
+	bool add_box(Box & result, Box &to_add)
+	{
+		if (result.w == 0)
+		{
+			result = to_add;
+			return true;
+		}
+		if ( result.x >= to_add.x || abs(result.y - to_add.y) > 10
+			|| (to_add.x > result.x + result.w + THRESHOLD))
+			return false;
+		result.w = to_add.w + to_add.x - result.x ;
+		result.h = std::max(result.h, to_add.h); 
+		return true;
+	}
+
 	void process_image(std::pair<std::string, cv::Mat> & image)
 	{
-		/*std::vector<std::string> test;
-		test.push_back("E:/bachelor_thesis/tabularOCR/test_images/img/9.jpg");
-		test.push_back("E:/bachelor_thesis/tabularOCR/test_images/img/2.jpg");
-		test.push_back("E:/bachelor_thesis/tabularOCR/test_images/img/3.jpg");
-		test.push_back("E:/bachelor_thesis/tabularOCR/test_images/img/73.jpg");
-		test.push_back("E:/bachelor_thesis/tabularOCR/test_images/img/13.jpg");
-		test.push_back("E:/bachelor_thesis/tabularOCR/test_images/img/8.jpg");
-		test.push_back("E:/bachelor_thesis/tabularOCR/test_images/img/5.jpg");
-		test.push_back("E:/bachelor_thesis/tabularOCR/test_images/img/table.jpg");
-		test.push_back("E:/bachelor_thesis/tabularOCR/test_images/img/11.jpg");
-		test.push_back("E:/bachelor_thesis/tabularOCR/test_images/img/7.jpg");
-		std::vector<std::string> name;
-		name.push_back("9");
-		name.push_back("2");
-		name.push_back("3");
-		name.push_back("73");
-		name.push_back("13");
-		name.push_back("8");
-		name.push_back("5");
-		name.push_back("table");
-		name.push_back("11");
-		name.push_back("7");
-		*/
-
-		Pix *img = pixRead("E:/bachelor_thesis/tabularOCR/test_images/img/5.jpg");
+		Pix *img = pixRead("E:/bachelor_thesis/tabularOCR/test_images/img/12.jpg");
+		img = pixConvert8To32(img);
 		tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
-		char *outText;
 		if (api->Init(NULL, "eng", tesseract::OcrEngineMode::OEM_DEFAULT))
 		{
 			fprintf(stderr, "Could not initialize tesseract.\n");
@@ -68,132 +59,46 @@ namespace ocr
 		api->SetVariable("textord_tablefind_recognize_tables", "true");
 		api->Recognize(0);
 
-		std::string out = "E:/bachelor_thesis/tabularOCR/5textline.jpg";
-		char *path = &out[0u];
-
-		Boxa * boxes = api->GetComponentImages(tesseract::RIL_TEXTLINE, false, NULL, NULL);
-		for (size_t i = 0; i < boxes->n; i++)
-		{
-			BOX* box = boxaGetBox(boxes, i, L_CLONE);
-			api->SetRectangle(box->x, box->y, box->w, box->h);
-			set_border(img, box, 255, 0, 0);
-			std::cout << i << ":" << box->x << ":" << box->y << ":" << box->w << ":" << box->h << ":" << ":" << std::endl;
-		}
-		pixWrite(path, img, IFF_PNG);
-
-		img = pixRead("E:/bachelor_thesis/tabularOCR/test_images/img/5.jpg");
-		api->SetImage(img);
-		api->Recognize(0);
-		Boxa * box2 = api->GetComponentImages(tesseract::RIL_WORD, false, NULL, NULL);
-		for (size_t i = 0; i < box2->n; i++)
-		{
-			BOX* box = boxaGetBox(box2, i, L_CLONE);
-			api->SetRectangle(box->x, box->y, box->w, box->h);
-			set_border(img, box, 0, 255, 0);
-			std::cout << i << ":" << box->x << ":" << box->y << ":" << box->w << ":" << box->h << ":" << ":" << std::endl;
-		}
-		out = "E:/bachelor_thesis/tabularOCR/5word.jpg";
-		path = &out[0u];
-		pixWrite(path, img, IFF_PNG);
-
-		img = pixRead("E:/bachelor_thesis/tabularOCR/test_images/img/5.jpg");
-		api->SetImage(img);
-		api->Recognize(0);
-		Boxa * box3 = api->GetComponentImages(tesseract::RIL_SYMBOL, false, NULL, NULL);
-		for (size_t i = 0; i < box3->n; i++)
-		{
-			BOX* box = boxaGetBox(box3, i, L_CLONE);
-			api->SetRectangle(box->x, box->y, box->w, box->h);
-			//char* ocrResult = api->GetUTF8Text();
-			//int conf = api->MeanTextConf();
-
-			set_border(img, box, 0, 0, 255);
-			std::cout << i << ":" << box->x << ":" << box->y << ":" << box->w << ":" << box->h << ":" << ":" << std::endl;
-		}
-		out = "E:/bachelor_thesis/tabularOCR/5symbol.jpg";
-		path = &out[0u];
-		pixWrite(path, img, IFF_PNG);
-
-		img = pixRead("E:/bachelor_thesis/tabularOCR/test_images/img/5.jpg");
-		api->SetImage(img);
-		api->Recognize(0);
-		Boxa * box4 = api->GetComponentImages(tesseract::RIL_BLOCK, false, NULL, NULL);
-		for (size_t i = 0; i < box4->n; i++)
-		{
-			BOX* box = boxaGetBox(box4, i, L_CLONE);
-			api->SetRectangle(box->x, box->y, box->w, box->h);
-			//char* ocrResult = api->GetUTF8Text();
-			set_border(img, box, 0, 0, 255);
-			std::cout << i << ":" << box->x << ":" << box->y << ":" << box->w << ":" << box->h << ":" << ":" << std::endl;
-		}
-		out = "E:/bachelor_thesis/tabularOCR/5block.jpg";
-		path = &out[0u];
-		pixWrite(path, img, IFF_PNG);
-
-		img = pixRead("E:/bachelor_thesis/tabularOCR/test_images/img/5.jpg");
-		api->SetImage(img);
-		api->Recognize(0);
-		Boxa * box5 = api->GetComponentImages(tesseract::RIL_PARA, false, NULL, NULL);
-		for (size_t i = 0; i < box5->n; i++)
-		{
-			BOX* box = boxaGetBox(box5, i, L_CLONE);
-			api->SetRectangle(box->x, box->y, box->w, box->h);
-			//char* ocrResult = api->GetUTF8Text();
-			set_border(img, box, 255, 0, 255);
-			std::cout << i << ":" << box->x << ":" << box->y << ":" << box->w << ":" << box->h << ":" << ":" << std::endl;
-		}
-		out = "E:/bachelor_thesis/tabularOCR/5para.jpg";
-		path = &out[0u];
-		pixWrite(path, img, IFF_PNG);
-
-
 		tesseract::ResultIterator* ri = api->GetIterator();
-		tesseract::PageIteratorLevel level = tesseract::RIL_BLOCK;
+		tesseract::PageIteratorLevel level = tesseract::RIL_SYMBOL;
 		if (ri == 0)
 			return;
 
+		BOX curr_box = {0,0,0,0,0};
+		std::string curr_word = "";
+
 		do {
-			const char* word = ri->GetUTF8Text(level);
+			const char* symbol = ri->GetUTF8Text(level);
 			float conf = ri->Confidence(level);
-			int x1, y1, x2, y2;
-			ri->BoundingBox(level, &x1, &y1, &x2, &y2);
-			auto type = ri->BlockType();
-			int r, g, b;
-			switch (type)
+			if (conf < 90)
+				continue;
+			int left, top, right, bottom;
+			ri->BoundingBox(level, &left, &top, &right, &bottom);
+			BOX new_box = { left, top, right - left, bottom - top };
+
+			if (!add_box(curr_box, new_box))
 			{
-			case PT_UNKNOWN: r = 0; g = 0; b = 0; break;
-			case PT_FLOWING_TEXT: r = 255; g = 0; b = 0; break;
-			case PT_HEADING_TEXT: r = 255; g = 0; b = 0; break;
-			case PT_PULLOUT_TEXT: r = 255; g = 0; b = 0; break;
-			case PT_EQUATION: r = 255; g = 255; b = 0; break;
-			case PT_INLINE_EQUATION: r = 255; g = 255; b = 0; break;
-			case PT_TABLE: r = 90; g = 60; b = 90; break;
-			case PT_VERTICAL_TEXT: r = 255; g = 0; b = 0; break;
-			case PT_CAPTION_TEXT: r = 255; g = 0; b = 0; break;
-			case PT_FLOWING_IMAGE: r = 0; g = 0; b = 255; break;
-			case PT_HEADING_IMAGE: r = 0; g = 0; b = 255; break;
-			case PT_PULLOUT_IMAGE: r = 0; g = 0; b = 255; break;
-			case PT_HORZ_LINE: r = 0; g = 255; b = 0; break;
-			case PT_VERT_LINE: r = 0; g = 255; b = 0; break;
-			case PT_NOISE: r = 0; g = 255; b = 255; break;
-			case PT_COUNT: r = 0; g = 0; b = 0; break;
+				set_border(img, &curr_box, 255, 0, 255);
+				std::cout << curr_word << std::endl;
+				curr_box = { 0,0,0,0,0 };
+				curr_word = "";
+				continue;
 			}
-			BOX box = { x1, y1, x2 - x1, y2 - y1 };
-			set_border(img, &box, r, g, b);
+			curr_word += symbol;
+			//auto type = ri->BlockType();
+			//BOX box = { x1, y1, x2 - x1, y2 - y1 };
+			//set_border(img, &box, 255, 0, 255);
+			delete[] symbol;
+		}
+		while (ri->Next(level));
 
-			delete[] word;
-		} while (ri->Next(level));
-		//std::string output_path = "E:/bachelor_thesis/tabularOCR/vystup.jpg";
-		//char *path = &output_path[0u];
 
-		out = "E:/bachelor_thesis/tabularOCR/5vystup.jpg";
-		path = &out[0u];
+		std::string out = "E:/bachelor_thesis/tabularOCR/vystup10.jpg";
+		char* path = &out[0u];
 		pixWrite(path, img, IFF_PNG);
 
-		//pixWrite(path, img, IFF_PNG);
-
 		// Get OCR result
-		outText = api->GetUTF8Text();
+		char* outText = api->GetUTF8Text();
 		printf("OCR output:\n%s", outText);
 
 		// Destroy used object and release memory
--- tabularOCR/src/ocr_process/process.h
+++ tabularOCR/src/ocr_process/process.h
@@ -5,12 +5,18 @@
 #include <baseapi.h>
 #include <renderer.h>
 #include <tablefind.h>
+#include <algorithm> 
 
 namespace ocr
 {
+
+	const size_t THRESHOLD = 10;
+
 	void set_border(PIX* img, BOX *box, int r, int g, int b);
 
 	Pix *matToPix(cv::Mat *mat);
+
+	bool add_box(Box & result, Box &to_add);
 
 	void process_image(std::pair<std::string, cv::Mat> & image);
 
